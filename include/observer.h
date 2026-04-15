ae57cb3e04e5d7cca10bdf5e49412a4633a6596d
liliangnike <liliangnike@126.com>
2026-04-15 06:18:14 +0000

    Define observer interface

--- include/observer.h
+++ include/observer.h
@@ -1,11 +1,28 @@
 #ifndef __OBSERVER_HEADER_FILE__
 #define __OBSERVER_HEADER_FILE__
 
+#include "process_info.h"
+
 enum class AlertType {
     CPU_HIGH,
     MEM_HIGH,
     CPU_NORMAL,
     MEM_NORMAL
+};
+
+struct AlertEvent {
+    AlertType alert_type;
+    process_info_t proc;
+    std::string message;
+    double value;
+    double threshold;
+};
+
+// abstract observer interface
+class AlertObserver {
+public:
+    virtual ~AlertObserver() = default;
+
 };
 
 #endif
